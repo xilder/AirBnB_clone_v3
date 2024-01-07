1cc440ea1ded5fc67c4c9c967eeb6caf1d8a1f11
xilder <olabowaleayobami@gmail.com>
2024-01-07 12:02:30 -0800

    Revert "updates"
    This reverts commit d6b4b4e1c56e96a5f0218ebdc9b9657d9e2a2383.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,7 +8,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route("/states", methods=["GET"], strict_slashes=False)
+@api_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_states():
     """
     gets all states from the database
