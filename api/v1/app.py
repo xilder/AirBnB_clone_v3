c17e33bcada41b452930f1000ae8b8fb6ab3ced6
xilder <olabowaleayobami@gmail.com>
2024-01-07 06:59:36 -0800

    handles the error 404 code

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,8 @@ import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
+from flask import jsonify
+from flask import make_response
 from models.place import Place
 from models.review import Review
 from models.state import State
@@ -27,7 +29,13 @@ def close_db(obj):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(err):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify(error="Not found"), 404)
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=host, port=int(port))
+    app.run(host=host, port=int(port), debug=True)
