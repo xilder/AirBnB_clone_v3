e8293c4e6890238157dd5666ddae13d0df3d79ab
xilder <olabowaleayobami@gmail.com>
2024-01-07 06:10:24 -0800

    adds stats route, app_views, in api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,24 @@
 blueprint for app_views
 """
 from api.v1.views import app_views
+from models.engine.db_storage import classes
+from flask import jsonify
+from models import storage
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def status():
     """
     app status
     """
     return {"status": "OK"}
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """
+    stats of each class
+    """
+    all_stat = {}
+    for cls in classes:
+        all_stat[cls.lower()] = storage.count(classes[cls])
+    return jsonify(all_stat)
