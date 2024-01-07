05b4fc25e66f07fff8ce06a5d4d195d139ab8f7f
xilder <olabowaleayobami@gmail.com>
2024-01-07 06:49:16 -0800

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,7 +21,9 @@ def stats():
     """
     stats of each class
     """
-    all_stat = {}
-    for cls in classes:
-        all_stat[cls.lower()] = storage.count(classes[cls])
-    return jsonify(all_stat)
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
