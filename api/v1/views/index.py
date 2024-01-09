976a27acbd3d9492b42131a26ca0803c9ee83a87
xilder <olabowaleayobami@gmail.com>
2024-01-08 22:32:53 -0800

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route("/status", methods=["GET"],strict_slashes=False)
 def status():
     """
     app status
@@ -16,7 +16,7 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def stats():
     """
     stats of each class
