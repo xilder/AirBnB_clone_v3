f3741fe9fcad9e18648005ef29af91364deb439f
xilder <olabowaleayobami@gmail.com>
2024-01-14 11:09:05 -0800

    updates 0-hbnb with thee cache

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,6 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+from uuid import uuid4
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -37,7 +38,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=uuid())
 
 
 if __name__ == "__main__":
